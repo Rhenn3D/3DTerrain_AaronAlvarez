8139f441d765933f3519a3ed6570f19cea010531
Rhenn3D <aalvarez@ctsformacion.com>
2025-11-08 16:19:07 +0100

    gh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,13 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
+    [Header("Ground Check")]
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
+
+    [Header ("Physics")]
+    [SerializeField] private float _gravityScale = 1;
+
     [Header("Movimiento")]
     [SerializeField] private float _playerSpeed = 6;
     [SerializeField] private float _jumpForce = 6;
@@ -13,6 +20,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector2 _moveAction;
     private InputAction _jumpInput;
     [SerializeField] private Vector2 _jumpAction;
+    [SerializeField] private Animator _animator;
+    
+    
+
 
 
 
@@ -27,6 +38,40 @@ public class PlayerController : MonoBehaviour
     {
         _moveAction = _moveInput.ReadValue<Vector2>();
     }
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(_moveAction.x, 0, _moveAction.y);
+        transform.Translate(move * _playerSpeed * Time.fixedDeltaTime, Space.World);
+        _animator.SetFloat("Speed", move.magnitude);
+    }
+
+    private void OnEnable()
+    {
+        _moveInput.Enable();
+        _jumpInput.Enable();
+    }
+    private void OnDisable()
+    {
+        _moveInput.Disable();
+        _jumpInput.Disable();
+    }
+
+    void Jump()
+    {
+        if ()
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+
+                _animator.SetBool("isJumping", true);
+            }
+        }
+    }
+
+
+
+
 
 
 }
