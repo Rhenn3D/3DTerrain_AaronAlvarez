0292365cdee9954f89e1d3e7c722b67bd0cba93b
Rhenn3D <aalvarez@ctsformacion.com>
2025-11-11 10:59:25 +0100

    Minimapa y salto por fin

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,15 @@ public class PlayerController : MonoBehaviour
 {
 
     [Header("Ground Check")]
-    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private Transform _sensor;
     [SerializeField] private LayerMask _groundLayer;
-
-    [Header ("Physics")]
-    [SerializeField] private float _gravityScale = 1;
+    [SerializeField] private float _sensorRadius = 2;
 
     [Header("Movimiento")]
     [SerializeField] private float _playerSpeed = 6;
     [SerializeField] private float _jumpForce = 6;
+    [SerializeField] private float _smoothTime = 0.2f;
+    private float _turnSmoothVelocity;
 
     [Header("Inputs")]
 
@@ -21,7 +21,14 @@ public class PlayerController : MonoBehaviour
     private InputAction _jumpInput;
     [SerializeField] private Vector2 _jumpAction;
     [SerializeField] private Animator _animator;
+
+    [Header("Gravedad")]
+    [SerializeField] private float _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
     
+    [Header("Otros")]
+    private CharacterController characterController;
+    private Transform _mainCamera;
     
 
 
@@ -29,24 +36,31 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        characterController = GetComponent<CharacterController>();
         _moveInput = InputSystem.actions["Move"];
         _jumpInput = InputSystem.actions["Jump"];
+        _animator = GetComponent<Animator>();
+
+        _mainCamera = Camera.main.transform;
     }
     
 
     void Update()
     {
         _moveAction = _moveInput.ReadValue<Vector2>();
+
+        if (_jumpInput.WasPressedThisFrame() && IsGrounded())
+        {
+            Jump();
+        }
+
+        Movement();
+
+        Gravity();
     }
 
-    void FixedUpdate()
-    {
-        Vector3 move = new Vector3(_moveAction.x, 0, _moveAction.y);
-        transform.Translate(move * _playerSpeed * Time.fixedDeltaTime, Space.World);
-        _animator.SetFloat("Speed", move.magnitude);
-    }
 
-    private void OnEnable()
+   /* private void OnEnable()
     {
         _moveInput.Enable();
         _jumpInput.Enable();
@@ -55,23 +69,60 @@ public class PlayerController : MonoBehaviour
     {
         _moveInput.Disable();
         _jumpInput.Disable();
+    }*/
+
+    void Gravity()
+    {
+        if (!IsGrounded())
+        {
+            _playerGravity.y += _gravity * Time.deltaTime;
+        }
+        else if (IsGrounded() && _playerGravity.y < 0)
+        {
+            _playerGravity.y = _gravity;
+            _animator.SetBool("IsJumping", false);
+        }
+        
+
+        characterController.Move(_playerGravity * Time.deltaTime);
     }
 
     void Jump()
     {
-        if ()
-        {
-            if (hit.collider.CompareTag("Ground"))
-            {
+        _animator.SetBool("IsJumping", true);
+        _playerGravity.y = Mathf.Sqrt(_jumpForce * -2 * _gravity);
+        characterController.Move(_playerGravity * Time.deltaTime);
+    }
 
-                _animator.SetBool("isJumping", true);
-            }
-        }
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(_sensor.position, _sensorRadius, _groundLayer);
+
     }
 
 
+    
 
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_moveAction.x, 0, _moveAction.y);
 
+        _animator.SetFloat("Vertical", _moveAction.y);
+        _animator.SetFloat("Horizontal", _moveAction.x);
+        if (direction != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _mainCamera.eulerAngles.y;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _smoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
 
-
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            characterController.Move(moveDirection.normalized * _playerSpeed * Time.deltaTime);
+        }
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(_sensor.position, _sensorRadius);
+    }
 }
